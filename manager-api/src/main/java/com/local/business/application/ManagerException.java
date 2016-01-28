50086f084cc76c84a58e5c768c9ac7e0b8faaabd
rafikbeldi07 <beldi.rafik.07@gmail.com>
2016-01-28 16:10:57 +0100

    commiting changes on eception

--- manager-api/src/main/java/com/local/business/application/ManagerException.java
+++ manager-api/src/main/java/com/local/business/application/ManagerException.java
@@ -4,4 +4,15 @@ public abstract class ManagerException extends RuntimeException {
 
 	private static final long serialVersionUID = -8170619680829917821L;
 
+	public ManagerException() {
+		super();
+	}
+
+	public ManagerException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ManagerException(String message) {
+		super(message);
+	}
 }
