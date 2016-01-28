72e89b967d571a2bb2f9ce02a2cf1769939cbc6f
rafikbeldi07 <beldi.rafik.07@gmail.com>
2016-01-28 16:01:06 +0100

    adding user model

---
+++ manager-api/src/main/java/com/local/business/application/ManagerException.java
@@ -0,0 +1,7 @@
+package com.local.business.application;
+
+public abstract class ManagerException extends RuntimeException {
+
+	private static final long serialVersionUID = -8170619680829917821L;
+
+}
---
+++ manager-api/src/main/java/com/local/business/domain/User.java
@@ -0,0 +1,14 @@
+package com.local.business.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -4259163172187942821L;
+
+}
